dad13cb4ac5e7ae07f1b313c6601cb7ed53197fb
Gagandeep Singh <gagandeep.12322960@lpu.in>
2026-01-26 15:19:47 +0530

    update all patterns

--- patterns.cpp
+++ patterns.cpp
@@ -50,8 +50,8 @@ int main(){
     cout<<endl;
 
     //full pyramid
-    for(int i=0;i<n;i++){
-        for(int j=1;j<n-i;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
             cout<<" ";
         }
         for(int k=0;k<2*i-1;k++){
@@ -62,7 +62,55 @@ int main(){
     cout<<endl;
 
     //inverted full pyramid
-     
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i-1;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=2*(n-i)+1;k++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    cout<<endl;
+
+
+    //half dimand shape (coblination of right half and inverted right half)
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    for(int i=0;i<=n;i++){
+        for(int j=1;j<n-i+1;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+
+    }
+    cout<<endl;
+    
+    
+    //Diamond Shape (combination of full pyreamid and inverted full pyramid)
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<2*i-1;k++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i-1;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=2*(n-i)+1;k++){
+            cout<<"*";
+        }
+        cout<<"\n";
+     }
+     cout<<endl;
 
 
 
